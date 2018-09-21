1cc74c1596781db5516ea55a1e849bf52bb15b20
jgchenu <jgchenu@foxmail.com>
2018-09-22 07:39:36 +0800

    删除原本的路由

--- router.js
+++ router.js
@@ -2,12 +2,8 @@
 const Router = require('koa-router')
 const router = new Router();
 const user = require('./routers/user')
-const home = require('./routers/home')
-const goods = require('./routers/goods')
 
 //配置路由
 router.use('/user', user.routes())
-router.use('/home', home.routes())
-router.use('/goods', goods.routes())
 
 module.exports = router;
