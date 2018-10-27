bd6343ddeac0dbfc7eaf5604855823664a59de96
jgchenu <jgchenu@foxmail.com>
2018-10-27 19:55:15 +0800

    更新了头像存储目录

--- controllers/user.js
+++ controllers/user.js
@@ -118,7 +118,7 @@ class UserController {
         const file = ctx.request.files.avatar; // 获取上传文件
         const reader = fs.createReadStream(file.path); // 创建可读流
         const ext = file.name.split('.').pop(); // 获取上传文件扩展名
-        const uploadUrl = `upload/${Math.random().toString()}.${ext}`;
+        const uploadUrl = `avatar/${Math.random().toString()}.${ext}`;
         const upStream = fs.createWriteStream(`static/${uploadUrl}`); // 创建可写流
         reader.pipe(upStream); // 可读流通过管道写入可写流
         const envHost = '';
