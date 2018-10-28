a26ce8728cad2c88227e1ae01af6e450abbcd407
jgchenu <jgchenu@foxmail.com>
2018-10-28 20:18:00 +0800

    修改了gitignore

--- middlewares/timeoutHandle.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (ctx, next) => {
-    ctx.req.setTimeout(120000);
-    ctx.res.setTimeout(120000);
-    await next();
-}
--- routers/tag.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router')
-const TagController = require('../controllers/tag')
-const router = new Router()
-
-//获取所有标签以及每个标签的总数
-router.get('/', TagController.getTag)
-router.get('/:name', TagController.getTagArticle)
-module.exports = router
