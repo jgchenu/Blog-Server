c7b1373bf97449e9b25ec77e74db72d7d097d3b1
jgchenu <jgchenu@foxmail.com>
2018-12-23 01:14:30 +0800

    修改了日志的打印

--- middlewares/logHandle.js
+++ middlewares/logHandle.js
@@ -2,5 +2,5 @@ module.exports = async (ctx, next) => {
     const start = new Date()
     await next()
     const ms = new Date() - start
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    console.log(`${ctx.method} ${ctx.url}   ${ctx.status}   ${ms}ms`)
 }
